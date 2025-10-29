86104697249f3cb9c8e8bed7177d4affe3558c5b
walterdogg <fandrewbuisness@yandex.ru>
2025-10-29 10:37:32 +0000

    First Commit

---
+++ main.c
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2014 - 2021, Nordic Semiconductor ASA
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form, except as embedded into a Nordic
+ *    Semiconductor ASA integrated circuit in a product or a software update for
+ *    such product, must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * 4. This software, with or without modification, must only be used with a
+ *    Nordic Semiconductor ASA integrated circuit.
+ *
+ * 5. Any software provided in binary form under this license must not be reverse
+ *    engineered, decompiled, modified and/or disassembled.
+ *
+ * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+/** @file
+ *
+ * @defgroup blinky_example_main main.c
+ * @{
+ * @ingroup blinky_example
+ * @brief Blinky Example Application main file.
+ *
+ * This file contains the source code for a sample application to blink LEDs.
+ *
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "nrf_delay.h"
+#include "boards.h"
+
+#define DEVICE_ID 6587
+
+void blink_led_count(int led_index, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        bsp_board_led_on(led_index);
+        nrf_delay_ms(200);
+        
+        bsp_board_led_off(led_index);
+        nrf_delay_ms(200);
+    }
+}
+
+void apply_device_id_pattern(int device_id)
+{
+    int digits[LEDS_NUMBER];
+    
+    digits[0] = (device_id / 1000) % 10;
+    digits[1] = (device_id / 100) % 10;
+    digits[2] = (device_id / 10) % 10;
+    digits[3] = device_id % 10;
+    
+    for (int i = 0; i < LEDS_NUMBER; i++)
+    {
+        blink_led_count(i, digits[i]);
+        
+        nrf_delay_ms(500);
+    }
+}
+
+/**
+ * @brief Function for application main entry.
+ */
+int main(void)
+{
+    bsp_board_init(BSP_INIT_LEDS);
+    bsp_board_leds_off();
+
+    
+    while (true)
+    {
+        apply_device_id_pattern(DEVICE_ID);
+        
+        nrf_delay_ms(3000);
+    }
+}
+
+/**
+ *@}
+ **/
