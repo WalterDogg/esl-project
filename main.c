ab6fd1808bdc909fb215fed5aaa051e728087d91
walterdogg <fandrewbuisness@yandex.ru>
2025-11-12 11:02:41 +0000

    Fourth commit

--- main.c
+++ main.c
@@ -1,106 +1,110 @@
-/**
- * Copyright (c) 2014 - 2021, Nordic Semiconductor ASA
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form, except as embedded into a Nordic
- *    Semiconductor ASA integrated circuit in a product or a software update for
- *    such product, must reproduce the above copyright notice, this list of
- *    conditions and the following disclaimer in the documentation and/or other
- *    materials provided with the distribution.
- *
- * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * 4. This software, with or without modification, must only be used with a
- *    Nordic Semiconductor ASA integrated circuit.
- *
- * 5. Any software provided in binary form under this license must not be reverse
- *    engineered, decompiled, modified and/or disassembled.
- *
- * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-/** @file
- *
- * @defgroup blinky_example_main main.c
- * @{
- * @ingroup blinky_example
- * @brief Blinky Example Application main file.
- *
- * This file contains the source code for a sample application to blink LEDs.
- *
- */
-
 #include <stdbool.h>
 #include <stdint.h>
 #include "nrf_delay.h"
-#include "boards.h"
+#include "nrf_gpio.h"
 
 #define DEVICE_ID 6587
 
-void blink_led_count(int led_index, int count)
+#define LED_PIN NRF_GPIO_PIN_MAP(0, 6)
+#define LED_R_PIN NRF_GPIO_PIN_MAP(0, 8)
+#define LED_G_PIN NRF_GPIO_PIN_MAP(1, 9)
+#define LED_B_PIN NRF_GPIO_PIN_MAP(0, 12)
+
+#define BUTTON_PIN NRF_GPIO_PIN_MAP(1, 6)
+
+const int digit1 = DEVICE_ID / 1000;
+const int digit2 = (DEVICE_ID / 100) % 10;
+const int digit3 = (DEVICE_ID / 10) % 10;
+const int digit4 = DEVICE_ID % 10;
+
+bool is_button_press()
 {
-    for (int i = 0; i < count; i++)
+    return nrf_gpio_pin_read(BUTTON_PIN) == 0;
+}
+
+
+void blink_led(int pin)
+{
+    nrf_gpio_pin_write(pin, 0);
+
+    for (int i = 0; i < 10; i++)
     {
-        bsp_board_led_on(led_index);
-        nrf_delay_ms(200);
-        
-        bsp_board_led_off(led_index);
-        nrf_delay_ms(200);
+        if (!is_button_press())
+        {
+            return;
+        }
+        nrf_delay_ms(100);
+    }
+
+    nrf_gpio_pin_write(pin, 1);
+}
+
+void init_led_arr(int *led_arr, int len_led_arr)
+{
+    int index = 0;
+
+    for (int i = 0; i < digit1; i++)
+    {
+        led_arr[index++] = LED_PIN;
+    }
+    for (int i = 0; i < digit2; i++)
+    {
+        led_arr[index++] = LED_R_PIN;
+    }
+    for (int i = 0; i < digit3; i++)
+    {
+        led_arr[index++] = LED_G_PIN;
+    }
+    for (int i = 0; i < digit4; i++)
+    {
+        led_arr[index++] = LED_B_PIN;
     }
 }
 
-void apply_device_id_pattern(int device_id)
+void init_pin()
 {
-    int digits[LEDS_NUMBER];
-    
-    digits[0] = (device_id / 1000) % 10;
-    digits[1] = (device_id / 100) % 10;
-    digits[2] = (device_id / 10) % 10;
-    digits[3] = device_id % 10;
-    
-    for (int i = 0; i < LEDS_NUMBER; i++)
-    {
-        blink_led_count(i, digits[i]);
-        
-        nrf_delay_ms(500);
-    }
+    nrf_gpio_cfg_output(LED_PIN);
+    nrf_gpio_cfg_output(LED_R_PIN);
+    nrf_gpio_cfg_output(LED_G_PIN);
+    nrf_gpio_cfg_output(LED_B_PIN);
+    nrf_gpio_cfg_input(BUTTON_PIN, NRF_GPIO_PIN_PULLUP);
 }
 
-/**
- * @brief Function for application main entry.
- */
+void leds_off()
+{
+    nrf_gpio_pin_write(LED_PIN, 1);
+    nrf_gpio_pin_write(LED_R_PIN, 1);
+    nrf_gpio_pin_write(LED_G_PIN, 1);
+    nrf_gpio_pin_write(LED_B_PIN, 1);
+}
+
 int main(void)
 {
-    bsp_board_init(BSP_INIT_LEDS);
-    bsp_board_leds_off();
+    init_pin();
 
-    
+    const int len_led_arr = digit1 + digit2 + digit3 + digit4;
+    int led_arr[len_led_arr];
+
+    init_led_arr(led_arr, len_led_arr);
+
+    int index = 0;
+
     while (true)
     {
-        apply_device_id_pattern(DEVICE_ID);
-        
-        nrf_delay_ms(3000);
+        if (is_button_press())
+        {
+            leds_off();
+            nrf_delay_ms(500);
+            blink_led(led_arr[index++]);
+            if (index >= len_led_arr)
+            {
+                nrf_delay_ms(1000);
+                index = 0;
+            }
+        }
+        else
+        {
+            nrf_delay_ms(100);
+        }
     }
 }
-
-/**
- *@}
- **/
